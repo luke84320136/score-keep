784884a72a83a23b8002ed4b497761509306b62a
Luke Lan <luke84320136@gmail.com>
2017-05-21 23:02:10 +0800

    import Mongo
    Mongodb

--- server/main.js
+++ server/main.js
@@ -1,7 +1,10 @@
-//import './../imports/utils.js';
-import {greetUser} from './../imports/utils';
-import{add} from './../imports/math';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players' ;
 
-
-console.log('Log from /server/main.js');
-console.log(add (7, 7));
+Meteor.startup(function(){
+  Players.insert({
+    name: 'Luke',
+    score:'80'
+  });
+  console.log(Players.find().fetch());
+});
